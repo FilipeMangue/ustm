b7cd370087912a8c0b2211b3826eb91dcf4a57b2
Filipe Mangue <43821186+FilipeMangue@users.noreply.github.com>
2023-07-29 18:28:29 +0200

    Estudo de MetodoRetorno2
    Estudo de MetodoRetorno 2

---
+++ MetodoRetorno2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MetodoRetorno2{
+	public static void main(String [] args){
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Digite a palavra: ");
+		String palavra = scn.nextLin();
+
+		System.out.println(palavra);
+	}
+	public static char returnLast3(String palavra){
+		for(int i = palavra.length
+	}
+}
